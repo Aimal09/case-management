3d2417a815d2c0931272299511659986808711c4
Ahsan Memon <Ahsan.Memon@linqlegal.com>
2025-05-06 18:59:47 +0500

    removed request param from case route

--- app/api/cases/[id]/route.ts
+++ app/api/cases/[id]/route.ts
@@ -1,8 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
 export async function GET(
-  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
