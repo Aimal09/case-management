3722ea1fc17b47a1ab6f17eadf50c9218e4f6d15
Ahsan Memon <ahsanmemon982@gmail.com>
2025-06-04 18:27:51 +0500

    next.config.ts basepath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  basePath: '/casemanagement',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
